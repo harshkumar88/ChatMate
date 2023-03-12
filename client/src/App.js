b16eb255d56a241873bf8a52c1194dfe8fa3256a
nakul412 <nakul1636.be20@chitkara.edu.in>
2023-03-12 21:04:50 +0530

    added navigation form login page

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,15 @@
 import React,{useState,useEffect} from "react";
 import { Routes,Route } from "react-router-dom";
-import Form from "./Components/Authentication/Form.js";
+
 import Chat from "./Components/Chats/Chat.jsx";
 import Chatting from "./Components/Chats/Main/Chatting.jsx";
+import Form from "./Components/Authentication/Form.js";
 
 function App() {
   return (
     <div>
     <Routes>
+      <Route exact path="/Form" element={<Form/>}/>
      <Route exact path='/' element={<Chat/>}/>
      <Route exact path='/Chatting' element={<Chatting/>}/>
     </Routes>
