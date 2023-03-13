1014f376905e239b276b91e235b4a8530f3776f9
krish160802 <krishkhera16@gmail.com>
2023-03-13 22:13:40 +0530

    Changes in form design  page

--- client/src/Components/Authentication/Form.js
+++ client/src/Components/Authentication/Form.js
@@ -42,7 +42,20 @@ const Form = () => {
 
    
     return (
-        <div className="App container-fluid">
+        <div className="App container-fluid area">
+                <ul className="circles">
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                </ul>
+
             <div className={change==false?"mt-5 outerWidth":"mt-5 outerWidth2"}>
                 <div className="main mx-auto innerWidth">
                     <input type="checkbox" id="chk" aria-hidden="true" />
