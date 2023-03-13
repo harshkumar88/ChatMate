b08585c84b64a217e3a79d1fa92bf02686add1cd
krish160802 <krishkhera16@gmail.com>
2023-03-13 12:03:21 +0530

    design

--- client/src/Components/Authentication/Login.js
+++ client/src/Components/Authentication/Login.js
@@ -54,7 +54,7 @@ const Login = () => {
                 <label htmlFor="chk" aria-hidden="true">
                     Login
                 </label>
-                <div className='w-75 mx-auto'>
+                <div className='w-75 mt-5 mx-auto'>
                     <input
                         type="text"
                         name="username"
@@ -65,7 +65,7 @@ const Login = () => {
                         onChange={getData}
                     />
                 </div>
-                <div className='w-75 mx-auto'>
+                <div className='w-75 mt-3 mx-auto'>
                     <input
                         type="email"
                         name="email"
@@ -76,7 +76,7 @@ const Login = () => {
                         onChange={getData}
                     />
                 </div>
-                <div className='w-75 mx-auto'>
+                <div className='w-75 mt-3 mx-auto'>
                     <input
                         type="password"
                         name="password"
@@ -87,8 +87,20 @@ const Login = () => {
                         onChange={getData}
                     />
                 </div>
-                <button>Login</button>
-            </form></div>
+
+                <button className='mt-3'>Login</button>
+            </form>
+            <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="16"
+                height="16"
+                fill="currentColor"
+                className="bi bi-google"
+                viewBox="0 0 16 16"
+            >
+                <path d="M15.545 6.558a9.42 9.42 0 0 1 .139 1.626c0 2.434-.87 4.492-2.384 5.885h.002C11.978 15.292 10.158 16 8 16A8 8 0 1 1 8 0a7.689 7.689 0 0 1 5.352 2.082l-2.284 2.284A4.347 4.347 0 0 0 8 3.166c-2.087 0-3.86 1.408-4.492 3.304a4.792 4.792 0 0 0 0 3.063h.003c.635 1.893 2.405 3.301 4.492 3.301 1.078 0 2.004-.276 2.722-.764h-.003a3.702 3.702 0 0 0 1.599-2.431H8v-3.08h7.545z" />
+            </svg>
+            </div>
     )
 }
 
