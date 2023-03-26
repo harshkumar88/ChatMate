445920db533b7fa25f95d2474710233fad6d2c3f
harshkumar <rkharshkumar88@gmail.com>
2023-03-26 16:47:54 +0530

    corrections in building

--- index.js
+++ index.js
@@ -7,13 +7,11 @@ require("./src/router.js")
 app.use(registerRouter)
 
 
-if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, 'Client', 'build')));
-    app.get('/*', (req, res) => {
-      res.sendFile(path.join(__dirname, 'Client', 'build', 'index.html'));
-    })
-  }
 
+  app.use(express.static(path.join(__dirname, 'Client', 'build')));
+  app.get('/*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'Client', 'build', 'index.html'));
+  })
 
 app.listen(port,(res,req)=>{
     console.log("I am running on port "+port)
