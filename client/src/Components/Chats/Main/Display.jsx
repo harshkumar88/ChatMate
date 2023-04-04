3fa9a9e2f735f02dbfdfdefe1140adeaa3b30073
krish160802 <krishkhera16@gmail.com>
2023-04-04 12:57:46 +0530

    tooltips

--- client/src/Components/Chats/Main/Display.jsx
+++ client/src/Components/Chats/Main/Display.jsx
@@ -61,11 +61,13 @@ const Display = ({change}) => {
     <div className='bi bi-plus-circle-fill sizeIcon ms-5 mx-1 bg-light borderR'></div>
     <div className='w-75'>
        <form onSubmit={sendMsg} className="d-flex heightSet">
+       <div className='inp w-100'>
        <input className='form-control bg-light' placeholder='send Message' onChange={(e)=>setText(e.target.value)} value={text}/>
+       <span className ='tooltiptxt'>Search</span>
+       </div>
        <div className='bi bi-emoji-smile sizeIcon ms-2 bg-light'></div>
        <button  type="submit" className='bg-light sizeIcon marginTop'><div className='bi bi-send-fill sizeIcon mx-1 bg-light '></div></button>
        </form>
-      
        </div>
     </div>
     </div>
