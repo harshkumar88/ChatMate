f0d4093baf43ec4ecbf75ce23cb1eeb1edf02b95
harshkumar <rkharshkumar88@gmail.com>
2023-04-10 15:47:44 +0530

    changes

--- client/src/App.js
+++ client/src/App.js
@@ -7,7 +7,7 @@ import Form from "./Components/Authentication/Form.js";
 import Forget from "./Components/Authentication/Forget.js";
 import Adduser from "./Components/Chats/AddCreate/Adduser.js";
 import Home from "./Components/HomePage/Home.jsx";
-// hello harsh bro
+//learn new feauters of github
 function App() {
   return (
     <div>
